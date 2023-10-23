1ad0ff8b4f42d892afbc7aeedba3eabfc013fd6a
DaveatCor <condaveat@gmail.com>
2023-10-23 13:48:09 +0700

    init project

---
+++ swift-crash-course/Variables.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+// Immutable = មិនអាចផ្លាស់ប្តូរបាន។
+let myName = "Daveat"
+// Mutable = អាចផ្លាស់ប្តូរបាន
+let yourName = "Foo"
+
+var name = [
+    myName,
+    yourName
+]
+
+name.append("Corn")
